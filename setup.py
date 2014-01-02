b42ebcf852e1e1812a4609e1ad054512155c2dfb
Alexander Oblovatniy <oblovatniy@gmail.com>
2014-01-02 16:38:59 +0200

    classifiers updated

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='wine-deamonizer',
-    version='1.0.0',
+    version='1.0.1',
     description='Run Windows processes under Wine as Unix daemons.',
     license='GPLv2',
     url='https://github.com/oblalex/Wine-deamonizer',
@@ -12,10 +12,13 @@ setup(
     classifiers = [
         'Development Status :: 5 - Production/Stable',
         'Intended Audience :: Developers',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
+        'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'License :: Free for non-commercial use',
         'Natural Language :: English',
-        'Operating System :: OS Independent',
+        'Operating System :: Unix',
         'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Libraries',
+        'Environment :: Console',
     ],
 )
